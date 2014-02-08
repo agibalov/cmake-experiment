b31bcedb97ad9eb6f63e0fb1c7f4e9f132994d56
loki2302 <loki2302@loki2302.me>
2014-02-08 23:32:44 +0400

    Added tests

--- build.sh
+++ build.sh
@@ -3,4 +3,13 @@ mkdir build
 cd build
 cmake ..
 make
+
+echo "*** Running tests"
+./hello-test
+echo "*** Done"
+
+echo "*** Running app"
+./hello
+echo "*** Done"
+
 cd ..
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+
+TEST(my_test_case, my_test_1) {
+	ASSERT_TRUE(true);
+}
+
+TEST(my_test_case, my_test_2) {
+	ASSERT_FALSE(false);
+}
